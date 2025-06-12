dbbdc36c898f248b3b41ee203c21a3f2db2c5a21
Janosia <janosia.banerjee03@gmail.com>
2025-06-12 20:11:52 +0530

    handling one request at a time, actual read and write instead of placeholder function

--- server.cpp
+++ server.cpp
@@ -20,24 +20,87 @@ static void die(const char *msg) {
     abort();
 }
 
-static void do_something(int connfd) {
-    char rbuf[64] = {};
-    ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
-    if (n < 0) {
-        msg("read() error");
-        return;
-    }
-    fprintf(stderr, "client says: %s\n", rbuf);
+const size_t k_max_msg = 4096;
 
-    char wbuf[] = "world";
-    write(connfd, wbuf, strlen(wbuf));
+static int32_t read_full(int fd, char *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = read(fd, buf, n);
+        if (rv <= 0) {
+            return -1;  // error, or unexpected EOF
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+// static void do_something(int connfd) {
+//     char rbuf[64] = {};
+//     ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
+//     if (n < 0) {
+//         msg("read() error");
+//         return;
+//     }
+//     fprintf(stderr, "client says: %s\n", rbuf);
+
+//     char wbuf[] = "world";
+//     write(connfd, wbuf, strlen(wbuf));
+// }
+
+static int32_t write_all(int fd, const char *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = write(fd, buf, n);
+        if (rv <= 0) {
+            return -1;  // error
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+static int32_t one_request(int connfd) {
+    // 4 bytes header
+    char rbuf[4 + k_max_msg];
+    errno = 0;
+    int32_t err = read_full(connfd, rbuf, 4);
+    if (err) {
+        msg(errno == 0 ? "EOF" : "read() error");
+        return err;
+    }
+
+    uint32_t len = 0;
+    memcpy(&len, rbuf, 4);  // assume little endian
+    if (len > k_max_msg) {
+        msg("too long");
+        return -1;
+    }
+
+    // request body
+    err = read_full(connfd, &rbuf[4], len);
+    if (err) {
+        msg("read() error");
+        return err;
+    }
+
+    // do something
+    fprintf(stderr, "client says: %.*s\n", len, &rbuf[4]);
+
+    // reply using the same protocol
+    const char reply[] = "world";
+    char wbuf[4 + sizeof(reply)];
+    len = (uint32_t)strlen(reply);
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], reply, len);
+    return write_all(connfd, wbuf, 4 + len);
 }
 
 int main() {
-    int fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (fd < 0) {
-        die("socket()");
-    }
+    int fd = socket(AF_INET6, SOCK_STREAM, 0);
+    if (fd < 0) die("socket()");
+    
 
     int val = 1;
     setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
@@ -57,12 +120,18 @@ int main() {
     
     while (true) {
         // accept
-        struct sockaddr_in client_addr = {};
+        struct sockaddr_in6 client_addr = {};
         socklen_t addrlen = sizeof(client_addr);
         int connfd = accept(fd, (struct sockaddr *)&client_addr, &addrlen);
         if (connfd < 0) continue;  
     
-        do_something(connfd);
+        while(true){
+            int32_t  err = one_request(connfd)
+
+            if(err) break;
+        }
+
+        // do_something(connfd);
         close(connfd);
     }
 
