2d089855b7e6b4f291b37af45b43ded8ae2b7659
Janosia <janosia.banerjee03@gmail.com>
2025-06-12 20:37:51 +0530

    multiple pipelined requests, dividing query into two parts

--- client.cpp
+++ client.cpp
@@ -51,36 +51,41 @@ static int32_t write_all(int fd, const char *buf, size_t n) {
     return 0;
 }
 
-const size_t k_max_msg = 4096;
+const size_t k_max_msg = 32<<20;
 
-static int32_t query(int fd, const char *text) {
-    uint32_t len = (uint32_t)strlen(text);
-    
+static void buf_append(vector<uint8_t> &buf, const uint8_t *data, size_t len) {
+    buf.insert(buf.end(), data, data + len);
+}
+
+static int32_t send_req(int fd, const uint8_t *text, size_t len) {
     if (len > k_max_msg) return -1;
 
-    char wbuf[4 + k_max_msg];
-    memcpy(wbuf, &len, 4);  // assume little endian
-    memcpy(&wbuf[4], text, len);
-    
-    if (int32_t err = write_all(fd, wbuf, 4 + len)) return err;
-    
+    vector<uint8_t>wbuf;
+    buf_append(wbuf, (const uint8_t *)&len, 4); // assume little endian
+    buf_append(wbuf, text, len);
+    return write_all(fd, wbuf.data(), wbuf.size());
+}
 
-    // 4 bytes header
-    char rbuf[4 + k_max_msg + 1];
+static int32_t read_res(int fd) {
+    // uint32_t len = (uint32_t)strlen(text);
+    vector<uint8_t>rbuf;
+    rbuf.resize(4);
     errno = 0;
+
     int32_t err = read_full(fd, rbuf, 4);
     if (err) {
         msg(errno == 0 ? "EOF" : "read() error");
         return err;
     }
-
-    memcpy(&len, rbuf, 4);  // assume little endian
+    uint32_t len =0;
+    memcpy(&len, rbuf.data(), 4);  // assume little endian
     if (len > k_max_msg) {
         msg("too long");
         return -1;
     }
 
     // reply body
+    rbuf.resize(4+len);
     err = read_full(fd, &rbuf[4], len);
     if (err) {
         msg("read() error");
@@ -89,7 +94,9 @@ static int32_t query(int fd, const char *text) {
 
     // do something
     // printf("server says: %.*s\n", len, &rbuf[4]);
-    cout << "server says: "<< string(&rbuf[4], len)<<endl;
+    // cout << "server says: "<< string(&rbuf[4], len)<<endl;
+    printf("len:%u data:%.*s\n", len, len < 100 ? len : 100, &rbuf[4]);
+    cout << "len:"<<len << " data " << string(&rbuf[4], len<100?len:100);
     return 0;
 }
 
@@ -121,14 +128,27 @@ int main() {
     
     // cout << "server says: " << rbuf << "\n";
     // multiple requests
-    int32_t err = query(fd, "hello1");
-    if (err) goto L_DONE;
+
     
-    err = query(fd, "hello2");
-    if (err) goto L_DONE;
+//     int32_t err = query(fd, "hello1");
+//     if (err) goto L_DONE;
     
-    err = query(fd, "hello3");
-    if (err) goto L_DONE;
+//     err = query(fd, "hello2");
+//     if (err) goto L_DONE;
+    
+//     err = query(fd, "hello3");
+//     if (err) goto L_DONE;
+// 
+    vector<string> query_list = {"hello1", "hello2", "hello3",string(k_max_msg, 'z'),"hello5",}
+    for (const string &s : query_list) {
+        int32_t err = send_req(fd, (uint8_t *)s.data(), s.size());
+        if (err) goto L_DONE;
+    }
+    for (size_t i = 0; i < query_list.size(); ++i) {
+        int32_t err = read_res(fd);
+        if (err) goto L_DONE;
+    }
+    
 L_DONE:
     close(fd);
     return 0;
