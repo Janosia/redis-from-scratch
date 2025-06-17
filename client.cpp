0a5bd3b8b1171cd328c183b2d2ad9068852b2f53
Janosia <janosia.banerjee03@gmail.com>
2025-06-17 11:39:33 +0000

    handled ipv6 address problem response is generated

--- client.cpp
+++ client.cpp
@@ -1,6 +1,3 @@
-#include <stdint.h>
-#include <stdlib.h>
-
 #include <errno.h>
 #include <unistd.h>
 
@@ -18,41 +15,46 @@ using namespace std;
 
 static void die(const char *msg) {
     int err = errno;
-    cerr << "[ " << err <<" ] "<< msg <<endl; 
+    cerr << "[" << err <<"] "<< msg <<endl; 
     abort();
 }
-struct sockaddr_in6 {
-    uint16_t        sin6_family;   // AF_INET6
-    uint16_t        sin6_port;     // port in big-endian
-    uint32_t        sin6_flowinfo; // ignore
-    struct in6_addr sin6_addr;     // IPv6
-    uint32_t        sin6_scope_id; // ignore
-};
 
-struct in6_addr {
-    vector<uint8_t>s6_addr(16);   // IPv6
-};
+// struct sockaddr_in6 {
+//     uint16_t        sin6_family;   // AF_INET6
+//     uint16_t        sin6_port;     // port in big-endian
+//     uint32_t        sin6_flowinfo; // ignore
+//     struct in6_addr sin6_addr;     // IPv6
+//     uint32_t        sin6_scope_id; // ignore
+// };
+
+// struct in6_addr {
+//     vector<uint8_t>s6_addr(16);   // IPv6
+// };
 
 static void msg(const char *msg) { cerr << msg <<endl;}
 
-static int32_t read_full(int fd, char *buf, size_t n) {
+static int32_t read_full(int fd, uint8_t *buf, size_t n) {
     while (n > 0) {
         ssize_t rv = read(fd, buf, n);
+        
         if (rv <= 0) return -1;  // error, or unexpected EOF
+        
         assert((size_t)rv <= n);
+        
         n -= (size_t)rv;
+        
         buf += rv;
     }
     return 0;
 }
 
-static int32_t write_all(int fd, const char *buf, size_t n) {
+static int32_t write_all(int fd, const uint8_t *buf, size_t n) {
     while (n > 0) {
         ssize_t rv = write(fd, buf, n);
-        if (rv <= 0) {
-            return -1;  // error
-        }
+        if (rv <= 0) return -1;  // error
+        
         assert((size_t)rv <= n);
+        
         n -= (size_t)rv;
         buf += rv;
     }
@@ -75,17 +77,18 @@ static int32_t send_req(int fd, const uint8_t *text, size_t len) {
 }
 
 static int32_t read_res(int fd) {
-    vector<char>rbuf;
+    vector<uint8_t>rbuf;
     rbuf.resize(4);
     errno = 0;
 
-    int32_t err = read_full(fd, &rbuf[4], 4);
+    int32_t err = read_full(fd, &rbuf[0], 4);
     if (err) {
         msg(errno == 0 ? "EOF" : "read() error");
         return err;
     }
     uint32_t len =0;
     memcpy(&len, rbuf.data(), 4);  // assume little endian
+    
     if (len > k_max_msg) {
         msg("too long");
         return -1;
@@ -98,7 +101,7 @@ static int32_t read_res(int fd) {
         msg("read() error");
         return err;
     }
-    cout << "Length : "<< len << " data " << string(&rbuf[4], len<100?len:100);
+    cout << "Length : "<< len << " data " << &rbuf[4];
     return 0;
 }
 
@@ -112,15 +115,21 @@ int main() {
     
     struct sockaddr_in6 addr = {};
     // binding to an address
+    // network to host 
     addr.sin6_family = AF_INET6;
-    addr.sin6_port = ntohs(1234);
-    addr.sin6_addr.s6_addr = ntohl(INADDR_LOOPBACK);  // 127.0.0.1
+    addr.sin6_port = htons(1234); 
+    addr.sin6_addr =in6addr_loopback;  // 127.0.0.1 Ipv6 already has 16 bytes address so ig no need to change 
     
     int rv = connect(fd, (const struct sockaddr *)&addr, sizeof(addr));
     
     if (rv) die("connect");
     
-    vector<string> query_list = {"hello1", "hello2", "hello3",string(k_max_msg, 'z'),"hello5",};
+    vector<string> query_list = {
+        "hello1", 
+        "hello2", 
+        "hello3",
+        string(k_max_msg, 'z'),
+        "hello5"};
     
     for(const string &s: query_list){
         int32_t err = send_req(fd, (uint8_t *)s.data(), s.size());
