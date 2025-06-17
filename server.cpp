170a1461aa7bc876f961ceb946086a5a7fb23a5b
Janosia <janosia.banerjee03@gmail.com>
2025-06-17 10:11:22 +0000

    server side includes cpp imports

--- server.cpp
+++ server.cpp
@@ -1,22 +1,38 @@
 #include <stdint.h>
 #include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
+
 #include <errno.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
 
+
+#include <iostream>
+#include <cassert>
+#include <string>
+#include <cstring>
+#include <vector>
 using namespace std;
 
-static void msg(const char *msg) {
-    fprintf(stderr, "%s\n", msg);
-}
+// struct sockaddr_in6 {
+//     uint16_t        sin6_family;   // AF_INET6
+//     uint16_t        sin6_port;     // port in big-endian
+//     uint32_t        sin6_flowinfo; // ignore
+//     struct in6_addr sin6_addr;     // IPv6
+//     uint32_t        sin6_scope_id; // ignore
+// };
+
+// struct in6_addr {
+//     vector<uint8_t> s6_addr(16);   // IPv6
+// };
+
+
+static void msg(const char *msg) { cerr << msg <<endl;}
 
 static void die(const char *msg) {
     int err = errno;
-    fprintf(stderr, "[%d] %s\n", err, msg);
+    cerr << err << " "<<msg <<endl;
     abort();
 }
 
@@ -34,19 +50,6 @@ static int32_t read_full(int fd, char *buf, size_t n) {
     }
     return 0;
 }
-
-// static void do_something(int connfd) {
-//     char rbuf[64] = {};
-//     ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
-//     if (n < 0) {
-//         msg("read() error");
-//         return;
-//     }
-//     fprintf(stderr, "client says: %s\n", rbuf);
-
-//     char wbuf[] = "world";
-//     write(connfd, wbuf, strlen(wbuf));
-// }
 
 static int32_t write_all(int fd, const char *buf, size_t n) {
     while (n > 0) {
@@ -85,9 +88,9 @@ static int32_t one_request(int connfd) {
         return err;
     }
 
-    // do something
-    fprintf(stderr, "client says: %.*s\n", len, &rbuf[4]);
-
+    cerr<< "client says: "<< &rbuf[4]<<endl;
+    
+    
     // reply using the same protocol
     const char reply[] = "world";
     char wbuf[4 + sizeof(reply)];
@@ -107,9 +110,10 @@ int main() {
 
     // bind
     struct sockaddr_in6 addr = {};
-    addr.sin_family = AF_INET6;
-    addr.sin_port = ntohs(1234);
-    addr.sin_addr.s_addr = ntohl(0);    // wildcard address 0.0.0.0
+    addr.sin6_family = AF_INET6;
+    addr.sin6_port = ntohs(1234);
+    // addr.sin6_addr.s6_addr = ntohs(0);    // wildcard address 0.0.0.0
+    
     int rv = bind(fd, (const struct sockaddr *)&addr, sizeof(addr));
     
     if (rv) die("bind()");
@@ -126,14 +130,10 @@ int main() {
         if (connfd < 0) continue;  
     
         while(true){
-            int32_t  err = one_request(connfd)
-
+            int32_t  err = one_request(connfd);
             if(err) break;
         }
-
-        // do_something(connfd);
         close(connfd);
     }
-
     return 0;
 }
