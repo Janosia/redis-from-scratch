e479bec793b26a47b8f27bff704bf9a438669730
Janosia <janosia.banerjee03@gmail.com>
2025-06-19 14:39:23 +0000

    self defined hash-table structures are present on this github file

---
+++ hashtable.h
@@ -0,0 +1,28 @@
+#pragma once 
+
+#include<iostream>
+#include <stdint.h>
+#include <stddef.h>
+
+struct HNode // instrusive list node
+{
+    HNode *next = NULL;
+    uint64_t hcode =0 ; // hashes
+};
+
+//fixed size hash table
+struct HTab{
+    HNode **tab = NULL;
+    size_t mask =0;
+    size_t size=0;
+};
+
+struct HMap{
+    HTab newer;
+    HTab older;
+    size_t migrate_pos=0;
+};
+
+HNode *hm_lookup(HMap *hmap, HNode *key, bool (*eq)(HNode *, HNode*));
+void *hm_insert(HTab *htab, HNode *node);
+HNode *h_delete(HTab *htab, HNode *key, bool(*eq) (HNode*, HNode*));
