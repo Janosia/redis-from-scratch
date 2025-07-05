587cdc607afc4d3fc97277ad96c9c949af050895
Janosia <janosia.banerjee03@gmail.com>
2025-07-05 05:11:27 +0000

    ranger query API added

--- zset.h
+++ zset.h
@@ -18,9 +18,14 @@ class ZNode{
 };
 
 
-// POINTER queries
+// POINTER query API 
 bool zset_insert(Zset *zset, const char *name, size_t len, double score);
 ZNode *znode_lookup(Zset *zset, const char *name, size_t len);
 void zset_delete(Zset *zset, ZNode *node);
 void zset_clear(Zset *zset);
 static void zset_update(Zset *zset, ZNode *node, double score);
+
+// RANGE QUERY API
+ZNode *zset_seekge(Zset *zset, double score, const char *name, size_t len);
+ZNode *znode_offset(ZNode *node , int64_t offset);
+
