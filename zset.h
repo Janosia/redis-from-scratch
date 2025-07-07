b364518eb9764d77e30f1f69483cc7330b57d82d
Janosia <janosia.banerjee03@gmail.com>
2025-07-07 07:51:50 +0000

    fixed comments

--- zset.h
+++ zset.h
@@ -17,15 +17,12 @@ class ZNode{
         char name[0]; // reduce memory allocations
 };
 
-
-// POINTER query API 
+// APIs 
 bool zset_insert(Zset *zset, const char *name, size_t len, double score);
 ZNode *zset_lookup(Zset *zset, const char *name, size_t len);
 void zset_delete(Zset *zset, ZNode *node);
 void zset_clear(Zset *zset);
 static void zset_update(Zset *zset, ZNode *node, double score);
-
-// RANGE QUERY API
 ZNode *zset_seekge(Zset *zset, double score, const char *name, size_t len);
 ZNode *znode_offset(ZNode *node , int64_t offset);
 
