18ec8c223fe13f42415e332e6d1c0f10411d837e
Janosia <janosia.banerjee03@gmail.com>
2025-07-09 08:23:09 +0000

    removed static keyword

--- src/server_helper.cpp
+++ src/server_helper.cpp
@@ -6,68 +6,25 @@
 
 using namespace std;
 
-static bool equality(HNode *lhs, HNode *rhs){
+bool equality(HNode *lhs, HNode *rhs){
     struct Entry *le = container_of(lhs, struct Entry, node);
     struct Entry *re = container_of(rhs, struct Entry, node);
     return le->key==re->key;
 }
 
-static bool entry_eq(HNode *lhs, HNode *rhs) {
+bool entry_eq(HNode *lhs, HNode *rhs) {
     struct Entry *le = container_of(lhs, struct Entry, node);
     struct Entry *re = container_of(rhs, struct Entry, node);
     return le->key == re->key;
 }
 
-static void out_nil(Buffer &out){
-    buf_append_u8(out, TAG_NIL);
-}
-
-static void out_str(Buffer &out, const char *s, size_t size){
-    buf_append_u8(out, TAG_STR);
-    buf_append_u32(out, (uint32_t)size);
-    buf_append(out, (const uint8_t *)s, size);
-}
-
-static void out_int(Buffer &out, int64_t val) {
-    buf_append_u8(out, TAG_INT);
-    buf_append_i64(out, val);
-}
-
-static void out_arr(Buffer &out, uint32_t n) {
-    buf_append_u8(out, TAG_ARR);
-    buf_append_u32(out, n);
-}
-
-static void out_dbl(Buffer &out, double val) {
-    buf_append_u8(out, TAG_DBL);
-    buf_append_dbl(out, val);
-}
-
-static void out_err(Buffer &out, uint32_t code, const string &msg) {
-    buf_append_u8(out, TAG_ERR);
-    buf_append_u32(out, code);
-    buf_append_u32(out, (uint32_t)msg.size());
-    buf_append(out, (const uint8_t *)msg.data(), msg.size());
-}
-
-static size_t out_begin_arr(Buffer &out) {
-    out.push_back(TAG_ARR);
-    buf_append_u32(out, 0);     
-    return out.size() - 4;     
-}
-
-static void out_end_arr(Buffer &out, size_t ctx, uint32_t n) {
-    assert(out[ctx - 1] == TAG_ARR);
-    memcpy(&out[ctx], &n, 4);
-}
-
-static bool str2dbl(const string &s, double &out) {
+bool str2dbl(const string &s, double &out) {
     char *endp = NULL;
     out = strtod(s.c_str(), &endp);
     return endp == s.c_str() + s.size() && !isnan(out);
 }
 
-static bool str2int(const string &s, int64_t &out) {
+bool str2int(const string &s, int64_t &out) {
     char *endp = NULL;
     out = strtoll(s.c_str(), &endp, 10);
     return endp == s.c_str() + s.size();
