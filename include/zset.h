5a012f8ee724d5da6a4be6c71ca1ad6128d086b5
Janosia <janosia.banerjee03@gmail.com>
2025-07-09 08:22:49 +0000

    removed static keyword

--- include/zset.h
+++ include/zset.h
@@ -22,7 +22,7 @@ bool zset_insert(Zset *zset, const char *name, size_t len, double score);
 ZNode *zset_lookup(Zset *zset, const char *name, size_t len);
 void zset_delete(Zset *zset, ZNode *node);
 void zset_clear(Zset *zset);
-static void zset_update(Zset *zset, ZNode *node, double score);
+void zset_update(Zset *zset, ZNode *node, double score);
 ZNode *zset_seekge(Zset *zset, double score, const char *name, size_t len);
 ZNode *znode_offset(ZNode *node , int64_t offset);
 
