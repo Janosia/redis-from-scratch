412ba3d74a86478735d4ce766161f85b18448424
Janosia <janosia.banerjee03@gmail.com>
2025-07-09 08:20:46 +0000

    doxygen comment

--- include/dl_list.h
+++ include/dl_list.h
@@ -5,12 +5,15 @@
 using namespace std;
 
 /* Using doubly linked list so that detach from middle is possible*/
+
 class Dlist{
     public:
         Dlist *prev = NULL;
         Dlist *next = NULL;
 };
 
+/* @brief : creation function;  
+*/
 inline void dlist_inint(Dlist *node){
     node->prev = node->next = node;
 }
@@ -27,10 +30,12 @@ inline void dlist_detach(Dlist *node){
 
 /*
 @brief : insertion in doubly linked list
-@param : target : node before which the new node will be added
-         rookie : node which will be added 
+@param  "target"  node before which the new node will be added
+@param  "rookie"  node which will be added 
 */
 inline void dlist_insert(Dlist *target, Dlist *rookie){
+    /*cout << target->prev << endl;
+    cout << rookie->next << endl;*/
     Dlist * prev = target->prev;
     prev->next = rookie;
     rookie->prev = prev;
