db97066754c8ea6145370a19514848a15a41cd6f
Janosia <janosia.banerjee03@gmail.com>
2025-07-12 07:53:42 +0000

    adding definition for set_ttl()

--- src/server_helper.cpp
+++ src/server_helper.cpp
@@ -3,7 +3,6 @@
 #include <cstring>
 
 #include "server_helper.h"
-
 using namespace std;
 
 bool equality(HNode *lhs, HNode *rhs){
@@ -28,4 +27,15 @@ bool str2int(const string &s, int64_t &out) {
     char *endp = NULL;
     out = strtoll(s.c_str(), &endp, 10);
     return endp == s.c_str() + s.size();
+}
+
+/*@brief Instantiation of Cache Time To Live(TTL)*/
+void set_ttl(Entry *ent, uint64_t ttl_ms){
+    if(ttl_ms < 0 && ent->h_indx != (size_t)-1){
+        HeapDelete(g_data.heap, ent->h_indx);
+        ent->h_indx = -1;
+    }else if (ttl_ms >=0){
+        uint64_t  expire_at = get_monotonic_msec() + ttl_ms;
+        HeapItem item = {expire_at, &ent->h_indx};
+    }
 }
