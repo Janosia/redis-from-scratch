b4be0e057b5d80ad59c05138a6d6dd6e3bedd68a
Janosia <janosia.banerjee03@gmail.com>
2025-07-12 17:09:12 +0000

    thread_pool header file

---
+++ include/thread_pool.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <stddef.h>
+#include <pthread.h>
+#include <vector>
+#include <deque>
+#include <iostream>
+
+using namespace std;
+
+class Work{
+public:
+    void (*f) (void *) = NULL;
+    void *arg = NULL;
+};
+
+class ThreadPool{
+public:
+    vector<pthread_t> threads;
+    deque<Work> queue;
+    pthread_mutex_t mu;
+    pthread_cond_t notempty;
+};
+
+void thread_pool_init(ThreadPool *tp, size_t num_threads);
+void thread_pool_queue(ThreadPool *tp, void (*f)(void *), void *arg);
