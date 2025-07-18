74171459a2ff3a3210a9bcea0d3dc40f26eb159b
Janosia <janosia.banerjee03@gmail.com>
2025-07-18 10:14:58 +0000

    converted structs to classes, added hm_clear API, made some functions inline

--- include/hashtable.h
+++ include/hashtable.h
@@ -1,25 +1,28 @@
 #pragma once 
-
+// C++ Imports
 #include <iostream>
+// C Imports
 #include <stdint.h>
 #include <stddef.h>
 
-struct HNode // instrusive list node
-{
-    HNode *next = NULL;
-    uint64_t hcode =0 ; 
+class HNode{
+    public:
+        HNode *next = NULL;
+        uint64_t hcode =0 ; 
 };
 
-struct HTab{
-    HNode **tab = NULL;
-    size_t mask =0;
-    size_t size=0;
+class HTab{
+    public:
+        HNode **tab = NULL;
+        size_t mask =0;
+        size_t size=0;
 };
 
-struct HMap{
-    HTab newer;
-    HTab older;
-    size_t migrate_pos=0;
+class HMap{
+    public:
+        HTab newer;
+        HTab older;
+        size_t migrate_pos=0;
 };
 
 class Hkey{
@@ -29,9 +32,20 @@ class Hkey{
         size_t len = 0;
 };
 
+// APIs
+inline void h_init(HTab *htab, size_t n){
+    // taking modulo using power of 2 since actual mod is CPU heavy operation
+    assert(n>0 && ((n-1)&n) == 0); 
+    htab->tab = (HNode **)calloc(n, sizeof(HNode *));
+    htab->mask = n-1;
+    htab->size = 0;
+};
 HNode *hm_lookup(HMap *hmap, HNode *key, bool (*eq)(HNode *, HNode*));
 void *hm_insert(HMap *hmap, HNode *node);
 HNode *h_delete(HMap *hmap, HNode *key, bool(*eq) (HNode*, HNode*));
+void   hm_clear(HMap *hmap);
 HNode* hm_delete(HMap *hmap, HNode *key, bool (*eq)(HNode * , HNode *));
 void hm_foreach(HMap *hmap, bool (*f)(HNode *, void *), void *arg);
-size_t hm_size(HMap *hmap);
+inline size_t hm_size(HMap *hmap){
+    return hmap->newer.size+hmap->older.size;
+};
